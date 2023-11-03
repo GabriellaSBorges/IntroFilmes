e4027b7cd83e2c0f8ca6813acc7af6624c918884
Gabriella Sirtoli Borges <77707319+GabriellaSBorges@users.noreply.github.com>
2023-11-03 19:03:10 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ function toggleMode() {
 
     html.classList.toggle('light')
 }
+
+function togglePipoca() {
+    const img = document.getElementById('imagem');
+
+    imagem.addEventListener('mouseover', function() {
+        imagem.src = 'pipoca.png';
+      });
+      
+      imagem.addEventListener('mouseout', function() {
+        imagem.src = 'pipoca1.png';
+      });
+}
